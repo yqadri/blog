ba72e4f37c057962ba222bd9e49e81a4213b05d7
yqadri <yasmin_qadri@yahoo.ca>
2014-04-03 12:20:22 -0600

    Blog application
    First assignment for Web Application Architecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'a43301dc416000d10cf0779dae65015a5c7809439b6731fd56a2a7239b8b04c4c49954e8c7e0cf066ce82aef9ece9b0b046210bf0b84b27bcb37415eb0247bbd'
